b42fa3be6de9e01e5cc014b041cf30e79a06c590
Dan Spalla <daniel@spalla.net>
2020-06-23 15:40:11 -0400

    Changed dependency script to install all dependencies for project. Moved all scripts to "/scripts" directory.

---
+++ scripts/project_install_dependencies.R
@@ -0,0 +1,12 @@
+# Installs all dependencies needed for the project.
+
+# Website
+install.packages("blogdown")
+blogdown::install_hugo()
+
+# For easy paths
+install.packages("here")
+
+# Interactive map
+install.packages("sf")
+install.packages("tmap")
--- website/website_install_local_dependencies.R
+++
@@ -1,5 +0,0 @@
-# Helper script to install dependencies to work on website locally
-# Installs Blogdown and Hugo
-
-install.packages("blogdown")
-blogdown::install_hugo()
